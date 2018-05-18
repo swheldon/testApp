50916f5cac3939ef03ec43a8a54cd8125e03d294
Scott Wheldon <Scott.Wheldon@gmail.com>
2018-05-17 22:15:51 -0700

    .

--- db/migrate/20180508164749_remove_images_from_estates.rb
+++
@@ -1,5 +0,0 @@
-class RemoveImagesFromEstates < ActiveRecord::Migration[5.1]
-  def change
-	remove_column :estates, :image
-  end
-end
--- db/migrate/20180515202742_remove_image_from_estates.rb
+++
@@ -1,6 +0,0 @@
-class RemoveImageFromEstates < ActiveRecord::Migration[5.1]
-  def change
-	remove_column :estates, :image
-	remove_column :estates, :image_data
-  end
-end
