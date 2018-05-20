df1f8a48cf8672081e19dbacc5ba2bf00eaa0754
Scott Wheldon <Scott.Wheldon@gmail.com>
2018-05-20 11:45:36 -0700

    .

--- config/initializers/email_setup.rb
+++ config/initializers/email_setup.rb
@@ -15,7 +15,7 @@ ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
   address:              'smtp.sendgrid.net',
   port:                 587,
-  domain:               'heroku.com',
+  domain:               'realestatecentral.herokuapp.com',
   user_name:            ENV["SENDGRID_USERNAME"],
   password:             ENV["SENDGRID_PASSWORD"],
   authentication:       'plain',
